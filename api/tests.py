1ba4efa934855193c56a7b3cf5acad7f38367b54
Ivan Antilevskiy <2045vanya@gmail.com>
2022-04-05 00:04:26 +0300

    CD

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Todo, User
 
-# Create your tests here.
+
+class TodoTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(id=1, name='Ivan')
+        Todo.objects.create(id=201, user_id=user, title='Wash car', completed=False)
+
+    def test_todo(self):
+        todo = Todo.objects.get(id=201)
+        self.assertEqual(todo.title, 'Wash car')
+        self.assertEqual(todo.completed, False)
